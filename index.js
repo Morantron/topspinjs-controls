f83cd83bbd0d13a404c51e94ea289f1e877be90c
Ferran Basora <fcsonline@gmail.com>
2015-08-17 23:49:35 +0200

    Improve initialization

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ left_pin = +process.env.TOPSPINJS_LEFT_PIN || 37;
 right_pin = +process.env.TOPSPINJS_RIGHT_PIN || 15;
 domain = process.env.TOPSPINJS_DOMAIN;
 
+if (!domain) {
+  throw new Error('TOPSPINJS_DOMAIN environment variable is mandatory');
+}
+
 endpoint = 'http://' + domain + '/api/games/current';
+
+console.log('Target domain: ' + domain);
 
 board = new five.Board({
   io: new raspi()
@@ -21,7 +27,9 @@ board.on("ready", function () {
   var left_button
     , right_button;
 
-  console.log('ready!');
+  console.log('Listening ' + left_pin + ' pin for left button');
+  console.log('Listening ' + right_pin + ' pin for right button');
+  console.log('Pins ready!');
 
   left_button = new five.Button(left_pin);
   right_button = new five.Button(right_pin);
